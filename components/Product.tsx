ce6127fcdd6a44eb0e421ea36f749e4379f224de
vishnu <kalanjiya.vishnu01@gmail.com>
2022-08-12 01:32:20 +0530

    prod create images bug

--- components/Product.tsx
+++ components/Product.tsx
@@ -8,6 +8,7 @@ export default function Product({
   product: any
   extra?: boolean
 }) {
+  let idx = 0
   const [thumb, setThumb] = useState(product.thumb)
   return (
     <div className="ml-8 mb-8 flex">
@@ -18,12 +19,33 @@ export default function Product({
           alt=""
         />
         <div className="mt-4">
-          <h1 className="text-2xl font-bold text-gray-700">{product.name}</h1>
-          <p className="mt-2 text-sm text-gray-700">Two sizes</p>
-          <div className="mt-3 flex space-x-4 p-1">
+          <div className="flex items-center justify-between">
+            <h1 className="text-2xl font-bold text-gray-700">{product.name}</h1>
+            <span
+              className="mt-3 flex w-8 transform cursor-pointer space-x-4 rounded-full border-4 p-1 transition duration-200 hover:scale-105"
+              onClick={() => {
+                setThumb(product.images[idx])
+                idx = (idx + 1) % product.images.length
+              }}
+            >
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                className="h-5 w-5"
+                viewBox="0 0 20 20"
+                fill="currentColor"
+              >
+                <path d="M8 5a1 1 0 100 2h5.586l-1.293 1.293a1 1 0 001.414 1.414l3-3a1 1 0 000-1.414l-3-3a1 1 0 10-1.414 1.414L13.586 5H8zM12 15a1 1 0 100-2H6.414l1.293-1.293a1 1 0 10-1.414-1.414l-3 3a1 1 0 000 1.414l3 3a1 1 0 001.414-1.414L6.414 15H12z" />
+              </svg>
+            </span>
+          </div>
+          <p className="mt-2 text-sm text-gray-700">
+            {product.size.length} sizes
+          </p>
+
+          {/* <div className="mt-3 flex space-x-4 p-1">
             <div className="transform cursor-pointer rounded-full border-4 p-1 transition duration-200 hover:scale-105 hover:border-green-200">
               <span
-                className="block h-6 w-6 rounded-full bg-green-400"
+                className="block h-6 w-6 rounded-full bg-trueGray-800"
                 onClick={() => {
                   console.log('green')
                   setThumb(product.images[0])
@@ -34,7 +56,7 @@ export default function Product({
             </div>
             <div className="transform cursor-pointer rounded-full border-4 p-1 transition duration-200 hover:scale-105 hover:border-blue-200">
               <span
-                className="block h-6 w-6 rounded-full bg-blue-400"
+                className="block h-6 w-6 rounded-full bg-trueGray-800"
                 onClick={() => {
                   console.log('green')
                   setThumb(product.images[1])
@@ -45,7 +67,7 @@ export default function Product({
             </div>
             <div className="transform cursor-pointer rounded-full border-4 p-1 transition duration-200 hover:scale-105 hover:border-yellow-200">
               <span
-                className="block h-6 w-6 rounded-full bg-yellow-400"
+                className="block h-6 w-6 rounded-full bg-trueGray-800"
                 onClick={() => {
                   console.log('green')
                   setThumb(product.images[2])
@@ -54,13 +76,13 @@ export default function Product({
                 {' '}
               </span>
             </div>
-          </div>
+          </div> */}
           <div className="mt-4 mb-2 flex justify-between pl-4 pr-2">
             <button className="block cursor-auto text-xl font-semibold text-gray-700">
               ₹{product.price}
             </button>
             <Link href={'product/' + product.id}>
-              <button className="block rounded-lg bg-sec-color-dark-1 py-2 px-6 text-lg font-semibold text-green-100 shadow transition duration-300 hover:text-white hover:shadow-md">
+              <button className="block rounded-lg bg-slate-800 py-2 px-4 text-sm  text-green-100 shadow transition duration-300 hover:text-white hover:shadow-md">
                 View
               </button>
             </Link>
--- pages/about.tsx
+++ pages/about.tsx
@@ -1,150 +1,258 @@
-import Head from 'next/head'
+// import Head from 'next/head'
 
-const about = () => {
-  return (
-    <div>
-      <Head>
-        <meta charSet="utf-8" />
-        <title />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+// const about = () => {
+//   return (
+//     <div>
 
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <section className="contact-section">
-        <div className="contact-bg">
-          <h3>Get in Touch with Us</h3>
-          <h2>About us</h2>
-          <div className="line">
-            <div />
-            <div />
-            <div />
-          </div>
-          <p className="text">
-            Aran windows and doors, started in the year 2019, is one of the
-            largest uPVC windows and doors brand. Aran windows and doors
-            specializes in providing customized and diverge range uPVC window
-            and door solutions.
-          </p>
-        </div>
-        <div className="contact-body">
-          <div className="contact-info">
-            <div>
-              <span>
-                <i className="fas fa-mobile-alt" />
-              </span>
-              <span>Phone No.</span>
-              <span className="text">+91-9445675569</span>
-            </div>
-            <div>
-              <span>
-                <i className="fas fa-envelope-open" />
-              </span>
-              <span>E-mail</span>
-              <span className="text">info@aranwindows.in</span>
-            </div>
-            <div>
-              <span>
-                <i className="fas fa-map-marker-alt" />
-              </span>
-              <span>Address</span>
-              <span className="text">
-                3/162C,M.G.R Nagar,Sankari Main
-                Road,Karuveppampatti,Tiruchengode-637209
-              </span>
-            </div>
-            <div>
-              <span>
-                <i className="fas fa-clock" />
-              </span>
-              <span>Opening Hours</span>
-              <span className="text">
-                Monday - Saturday (9:00 AM to 8:00 PM)
-              </span>
-            </div>
-          </div>
-          <div className="contact-form">
+//       <Head>
+//         <meta charSet="utf-8" />
+//         <title />
+//         <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+
+//         <meta name="viewport" content="width=device-width, initial-scale=1" />
+//       </Head>
+//       <section classNameName="contact-section">
+//         <div classNameName="contact-bg">
+//           <h3>Get in Touch with Us</h3>
+//           <h2>About us</h2>
+//           <div classNameName="line">
+//             <div />
+//             <div />
+//             <div />
+//           </div>
+//           <p classNameName="text">
+//             Aran windows and doors, started in the year 2019, is one of the
+//             largest uPVC windows and doors brand. Aran windows and doors
+//             specializes in providing customized and diverge range uPVC window
+//             and door solutions.
+//           </p>
+//         </div>
+//         <div classNameName="contact-body">
+//           <div classNameName="contact-info">
+//             <div>
+//               <span>
+//                 <i classNameName="fas fa-mobile-alt" />
+//               </span>
+//               <span>Phone No.</span>
+//               <span classNameName="text">+91-9445675569</span>
+//             </div>
+//             <div>
+//               <span>
+//                 <i classNameName="fas fa-envelope-open" />
+//               </span>
+//               <span>E-mail</span>
+//               <span classNameName="text">info@aranwindows.in</span>
+//             </div>
+//             <div>
+//               <span>
+//                 <i classNameName="fas fa-map-marker-alt" />
+//               </span>
+//               <span>Address</span>
+//               <span classNameName="text">
+//                 3/162C,M.G.R Nagar,Sankari Main
+//                 Road,Karuveppampatti,Tiruchengode-637209
+//               </span>
+//             </div>
+//             <div>
+//               <span>
+//                 <i classNameName="fas fa-clock" />
+//               </span>
+//               <span>Opening Hours</span>
+//               <span classNameName="text">
+//                 Monday - Saturday (9:00 AM to 8:00 PM)
+//               </span>
+//             </div>
+//           </div>
+//           <div classNameName="contact-form">
+//             <form>
+//               <div>
+//                 <input
+//                   type="text"
+//                   classNameName="form-control"
+//                   placeholder="First Name"
+//                 />
+//                 <input
+//                   type="text"
+//                   classNameName="form-control"
+//                   placeholder="Last Name"
+//                 />
+//               </div>
+//               <div>
+//                 <input
+//                   type="email"
+//                   classNameName="form-control"
+//                   placeholder="E-mail"
+//                 />
+//                 <input
+//                   type="text"
+//                   classNameName="form-control"
+//                   placeholder="Phone"
+//                 />
+//               </div>
+//               <textarea
+//                 rows={5}
+//                 placeholder="Message"
+//                 classNameName="form-control"
+//                 defaultValue={''}
+//               />
+//               <input
+//                 type="submit"
+//                 classNameName="send-btn"
+//                 defaultValue="send message"
+//               />
+//             </form>
+//             <div>
+//               <img src="/contact-png.png" alt="contact" />
+//             </div>
+//           </div>
+//         </div>
+//         <div classNameName="map">
+//           <iframe
+//             src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3911.3595768149244!2d77.8627673!3d11.381408599999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba96197a90a4deb%3A0x40674259b8e1b20f!2sAWD%20~%20Aran%20Windows%20and%20Doors(Production%20Unit)!5e0!3m2!1sen!2sin!4v1660201421064!5m2!1sen!2sin"
+//             width="100%"
+//             height={500}
+//             style={{ border: '0' }}
+//             allowFullScreen
+//             loading="lazy"
+//             referrerPolicy="no-referrer-when-downgrade"
+//           />
+//         </div>
+//         <div classNameName="contact-footer">
+//           <h3>Follow Us</h3>
+//           <div classNameName="social-links">
+//             <a
+//               href="https://m.facebook.com/1064784970359862/"
+//               classNameName="fab fa-facebook-f"
+//             />
+//             <a href="#" classNameName="fab fa-twitter" />
+//             <a
+//               href="https://www.instagram.com/aran_windows/?hl=en"
+//               classNameName="fab fa-instagram"
+//             />
+//           </div>
+//           <br />
+//           <div classNameName="et_pb_text_inner">
+//             <p
+//               style={{
+//                 textAlign: 'center',
+//                 color: 'aliceblue',
+//               }}
+//             >
+//               Copyright © 2022, Aran Windows and Doors
+//               <br />
+//               All Rights Reserved.
+//             </p>
+//           </div>
+//         </div>
+//       </section>
+//     </div>
+//   )
+// }
+// export default about
+export default () => (
+  <>
+    <div className="container my-24 mx-auto px-6">
+      <section className="mb-32 text-gray-800">
+        <div className="grid gap-4 lg:mb-0 lg:grid-cols-2 lg:gap-x-12">
+          <div className="mb-12 lg:mb-0">
+            <h2 className="mb-6 text-3xl font-bold">
+              Frequently asked questions
+            </h2>
+
+            <p className="mb-12 text-gray-500">
+              Didn't find your answer in the FAQ? Contact our sales team.
+            </p>
+
             <form>
-              <div>
+              <div className="form-group mb-6">
                 <input
                   type="text"
-                  className="form-control"
-                  placeholder="First Name"
-                />
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Last Name"
+                  className="form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+                  id="exampleInput7"
+                  placeholder="Name"
                 />
               </div>
-              <div>
+              <div className="form-group mb-6">
                 <input
                   type="email"
-                  className="form-control"
-                  placeholder="E-mail"
-                />
-                <input
-                  type="text"
-                  className="form-control"
-                  placeholder="Phone"
+                  className="form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+                  id="exampleInput8"
+                  placeholder="Email address"
                 />
               </div>
-              <textarea
-                rows={5}
-                placeholder="Message"
-                className="form-control"
-                defaultValue={''}
-              />
-              <input
+              <div className="form-group mb-6">
+                <textarea
+                  className="form-control m-0 block w-full rounded border border-solid border-gray-300 bg-white bg-clip-padding px-3 py-1.5 text-base font-normal text-gray-700 transition ease-in-out focus:border-blue-600 focus:bg-white focus:text-gray-700 focus:outline-none"
+                  id="exampleFormControlTextarea13"
+                  rows={3}
+                  placeholder="Message"
+                ></textarea>
+              </div>
+              <div className="form-group form-check mb-6 text-center">
+                <input
+                  type="checkbox"
+                  className="form-check-input mt-1 mr-2 h-4 w-4 cursor-pointer appearance-none rounded-sm border border-gray-300 bg-white bg-contain bg-center bg-no-repeat align-top transition duration-200 checked:border-blue-600 checked:bg-blue-600 focus:outline-none"
+                  id="exampleCheck87"
+                  checked
+                />
+                <label
+                  className="form-check-label inline-block text-gray-800"
+                  htmlFor="exampleCheck87"
+                >
+                  Send me a copy of this message
+                </label>
+              </div>
+              <button
                 type="submit"
-                className="send-btn"
-                defaultValue="send message"
-              />
+                className="w-full rounded bg-blue-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
+              >
+                Send
+              </button>
             </form>
-            <div>
-              <img src="/contact-png.png" alt="contact" />
-            </div>
           </div>
-        </div>
-        <div className="map">
-          <iframe
-            src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3911.3595768149244!2d77.8627673!3d11.381408599999999!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3ba96197a90a4deb%3A0x40674259b8e1b20f!2sAWD%20~%20Aran%20Windows%20and%20Doors(Production%20Unit)!5e0!3m2!1sen!2sin!4v1660201421064!5m2!1sen!2sin"
-            width="100%"
-            height={500}
-            style={{ border: '0' }}
-            allowFullScreen
-            loading="lazy"
-            referrerPolicy="no-referrer-when-downgrade"
-          />
-        </div>
-        <div className="contact-footer">
-          <h3>Follow Us</h3>
-          <div className="social-links">
-            <a
-              href="https://m.facebook.com/1064784970359862/"
-              className="fab fa-facebook-f"
-            />
-            <a href="#" className="fab fa-twitter" />
-            <a
-              href="https://www.instagram.com/aran_windows/?hl=en"
-              className="fab fa-instagram"
-            />
-          </div>
-          <br />
-          <div className="et_pb_text_inner">
-            <p
-              style={{
-                textAlign: 'center',
-                color: 'aliceblue',
-              }}
-            >
-              Copyright © 2022, Aran Windows and Doors
-              <br />
-              All Rights Reserved.
+
+          <div className="mb-6 md:mb-0">
+            <p className="mb-4 font-bold">
+              Anim pariatur cliche reprehenderit?
+            </p>
+            <p className="mb-12 text-gray-500">
+              Lorem ipsum dolor sit amet consectetur, adipisicing elit. Sunt
+              autem numquam dolore molestias aperiam culpa alias veritatis
+              architecto eos, molestiae vitae ex eligendi libero eveniet
+              dolorem, doloremque rem aliquid perferendis.
+            </p>
+
+            <p className="mb-4 font-bold">
+              Non cupidatat skateboard dolor brunch?
+            </p>
+            <p className="mb-12 text-gray-500">
+              Distinctio corporis, iure facere ducimus quos consectetur ipsa ut
+              magnam autem doloremque ex! Id, sequi. Voluptatum magnam sed fugit
+              iusto minus et suscipit? Minima sunt at nulla tenetur, numquam
+              unde quod modi magnam ab deserunt ipsam sint aliquid dolores
+              libero repellendus cupiditate mollitia quidem dolorem odit
+            </p>
+
+            <p className="mb-4 font-bold">
+              Praesentium voluptatibus temporibus consequatur non aspernatur?
+            </p>
+            <p className="mb-12 text-gray-500">
+              Minima sunt at nulla tenetur, numquam unde quod modi magnam ab
+              deserunt ipsam sint aliquid dolores libero repellendus cupiditate
+              mollitia quidem dolorem.
+            </p>
+
+            <p className="mb-4 font-bold">
+              Voluptatum magnam sed fugit iusto minus et suscipit?
+            </p>
+            <p className="mb-12 text-gray-500">
+              Laudantium perferendis, est alias iure ut veniam suscipit dolorem
+              fugit. Et ipsam corporis earum ea ut quae cum non iusto blanditiis
+              ipsum dolor eius reiciendis, velit adipisci quas.
             </p>
           </div>
         </div>
       </section>
     </div>
-  )
-}
-export default about
+  </>
+)
